19ff54435ae0271b31435a1f6dcf7238028a0f9d
zulkarnainfaizan@gmail.com <zulkarnainfaizan@gmail.com>
2026-08-15 23:58:53 +0530

    connect the front end to backend

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -10,6 +10,9 @@ const port = process.env.PORT || 3000;
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
+app.get('/api/hello', (req: Request, res: Response) => {
+  res.json({ message: 'Hello World!' });
+});
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
