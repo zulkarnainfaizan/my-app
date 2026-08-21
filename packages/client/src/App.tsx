098abed5d94fc369e841c2f48b1942cf4501b93d
zulkarnainfaizan@gmail.com <zulkarnainfaizan@gmail.com>
2026-08-21 10:49:33 +0530

    Set up Husky

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
    return (
       <div className="p-4">
          <p className="font-bold text-3xl">{message}</p>
-         <Button>Click me</Button>
+         <Button>Click me!</Button>
       </div>
    );
 }
